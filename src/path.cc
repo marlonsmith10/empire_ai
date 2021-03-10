0297e765164bb44516ac87aa45aeb9a885e4f764
Marlon Smith <marlon.smith10@gmail.com>
2021-03-10 14:58:22 -0500

    Store node objects instead of pointers in node containers

--- src/path.cc
+++ src/path.cc
@@ -11,12 +11,12 @@ using namespace EmpireAI;
 
 
 Path::Path(const TileIndex start, const TileIndex end)
-: m_end_tile_index(end)
+: m_start_tile_index(start), m_end_tile_index(end)
 {
 	// Create an open node at the start
-	m_current_node = m_start_node = get_node(start);
-	m_start_node->f = m_start_node->h;
-	open_node(m_current_node);
+	Node start_node = get_node(start);
+	start_node.f = start_node.h;
+	open_node(start_node);
 
 	m_status = IN_PROGRESS;
 }
@@ -33,46 +33,47 @@ Path::Status Path::find(const uint16_t max_node_count)
 	for(uint16 node_count = 0; node_count < max_node_count; node_count++)
 	{
 		// Get the cheapest open node
-		m_current_node = cheapest_open_node();
+		bool open_node_available = false;
+		Node current_node = cheapest_open_node(open_node_available);
 
 		// If there are no open nodes, the path is unreachable
-		if(m_current_node == nullptr)
+		if(!open_node_available)
 		{
 			m_status = UNREACHABLE;
 			break;
 		}
 
         // Mark the current node as closed
-	    close_node(m_current_node);
+	    close_node(current_node);
 
 	    // If we've reached the destination, return true
-	    if(m_current_node->tile_index == m_end_tile_index)
+	    if(current_node.tile_index == m_end_tile_index)
 	    {
 	    	m_status = FOUND;
 	        break;
 	    }
 
         // Calculate the f, h, g, values of the 4 surrounding nodes
-	    parse_adjacent_tile(m_current_node, 1, 0);
-	    parse_adjacent_tile(m_current_node, -1, 0);
-	    parse_adjacent_tile(m_current_node, 0, 1);
-	    parse_adjacent_tile(m_current_node, 0, -1);
+	    parse_adjacent_tile(current_node, 1, 0);
+	    parse_adjacent_tile(current_node, -1, 0);
+	    parse_adjacent_tile(current_node, 0, 1);
+	    parse_adjacent_tile(current_node, 0, -1);
 	}
 
 	return m_status;
 }
 
 
-void Path::parse_adjacent_tile(Node* const current_node, const int8 x, const int8 y)
+void Path::parse_adjacent_tile(const Node& current_node, const int8 x, const int8 y)
 {
-    TileIndex adjacent_tile_index = current_node->tile_index + ScriptMap::GetTileIndex(x, y);
+    TileIndex adjacent_tile_index = current_node.tile_index + ScriptMap::GetTileIndex(x, y);
 
-    Node* adjacent_node = get_node(adjacent_tile_index);
+    Node adjacent_node = get_node(adjacent_tile_index);
 
     // Check to see if this tile can be used as part of the path
     if(nodes_can_connect_road(current_node, adjacent_node))
     {
-        if(adjacent_node->update_costs(current_node))
+        if(adjacent_node.update_costs(current_node))
         {
             open_node(adjacent_node);
         }
@@ -84,23 +85,25 @@ void Path::parse_adjacent_tile(Node* const current_node, const int8 x, const int
 }
 
 
-bool Path::nodes_can_connect_road(const Node* const node_from, const Node* const node_to) const
+bool Path::nodes_can_connect_road(const Node& node_from, const Node& node_to)
 {
 	// The start node doesn't connect to a previous node, so we can't check it for the correct slope.
 	// The pathfinder can only ensure that the next node in the path can connect to the start node.
-	if(node_from->previous_node == nullptr)
+	if(node_from.previous_tile_index == INVALID_TILE)
 	{
 		return true;
 	}
 
-	int32 supports_road = ScriptRoad::CanBuildConnectedRoadPartsHere(node_from->tile_index, node_from->previous_node->tile_index, node_to->tile_index);
+	Node node_from_previous = get_node(node_from.previous_tile_index);
+
+	int32 supports_road = ScriptRoad::CanBuildConnectedRoadPartsHere(node_from.tile_index, node_from_previous.tile_index, node_to.tile_index);
 
 	if(supports_road <= 0)
 	{
 		return false;
 	}
 
-	if(!ScriptTile::IsBuildable(node_to->tile_index) && !ScriptRoad::IsRoadTile(node_to->tile_index))
+	if(!ScriptTile::IsBuildable(node_to.tile_index) && !ScriptRoad::IsRoadTile(node_to.tile_index))
 	{
 		return false;
 	}
@@ -109,46 +112,48 @@ bool Path::nodes_can_connect_road(const Node* const node_from, const Node* const
 }
 
 
-Path::Node* Path::cheapest_open_node()
+Path::Node Path::cheapest_open_node(bool& success)
 {
+	success = false;
+
 	// While there are open nodes available
 	while(!m_open_nodes.empty())
 	{
 		// Remove the cheapest node from the open nodes list
-		Node* current_node = m_open_nodes.top();
+		Node current_node = m_open_nodes.top();
 		m_open_nodes.pop();
 
 		// If this node has already been closed, skip to the next one. Duplicates are expected
 		// here because get_node() doesn't check for duplicates for performance reasons.
-		if(m_closed_nodes.find(current_node->tile_index) != m_closed_nodes.end())
+		if(m_closed_nodes.find(current_node.tile_index) != m_closed_nodes.end())
 		{
-			delete current_node;
 			continue;
 		}
 
+		success = true;
 		return current_node;
 	}
 
 	// There are no more open nodes
-	return nullptr;
+	return Node();
 }
 
 
-Path::Node* Path::get_node(const TileIndex tile_index)
+Path::Node Path::get_node(const TileIndex tile_index)
 {
     // If the node is not closed, create a new one.
 	// Duplicate open nodes are considered an acceptable tradeoff since it's not easy to search std::priority_queue for
 	// an already existing open node
     if(m_closed_nodes.find(tile_index) == m_closed_nodes.end())
     {
-    	return new Node(tile_index, ScriptMap::DistanceManhattan(tile_index, m_end_tile_index));
+    	return Node(tile_index, ScriptMap::DistanceManhattan(tile_index, m_end_tile_index));
     }
 
     return m_closed_nodes.at(tile_index);
 }
 
 
-void Path::open_node(Node* node)
+void Path::open_node(const Node& node)
 {
 	// Push the node into the open node list. Does not check open nodes, instead allowing
 	// duplicates to be created in the open node priority queue, since checking for already open nodes is slower
@@ -156,19 +161,19 @@ void Path::open_node(Node* node)
 	m_open_nodes.push(node);
 
 	// Remove the node from the closed list
-	m_closed_nodes.erase(node->tile_index);
+	m_closed_nodes.erase(node.tile_index);
 }
 
 
-void Path::close_node(Node* node)
+void Path::close_node(const Node& node)
 {
-    m_closed_nodes[node->tile_index] = node;
+    m_closed_nodes[node.tile_index] = node;
 }
 
 
-bool Path::Node::update_costs(Node* const adjacent_node)
+bool Path::Node::update_costs(const Node& adjacent_node)
 {
-    int32 new_g = adjacent_node->g + 1;
+    int32 new_g = adjacent_node.g + 1;
 
     int32 new_f = new_g + h;
 
@@ -178,27 +183,9 @@ bool Path::Node::update_costs(Node* const adjacent_node)
     {
         g = new_g;
         f = new_f;
-        previous_node = adjacent_node;
+        previous_tile_index = adjacent_node.tile_index;
         return true;
     }
 
     return false;
-}
-
-
-Path::~Path()
-{
-	// std::priority_queue doesn't provide an iterator, so pop all values and delete
-	while(!m_open_nodes.empty())
-	{
-		// Remove the cheapest node from the open nodes list
-		delete m_open_nodes.top();
-		m_open_nodes.pop();
-	}
-
-	// Get every std::pair from the map, delete the Node (which is the second element)
-    for(const std::pair<TileIndex, Node*>& pair : m_closed_nodes)
-    {
-        delete pair.second;
-    }
 }
