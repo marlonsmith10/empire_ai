3d20771b13379bd2ba1d34ad4de2a59bd2abbed9
Marlon Smith <marlon.smith10@gmail.com>
2021-06-28 16:42:32 -0400

    DecisionEngine uses GOF State pattern instead of large switch statementc++ sw

--- src/ai/decision_engine.cc
+++ src/ai/decision_engine.cc
@@ -14,96 +14,173 @@ using namespace EmpireAI;
 
 
 DecisionEngine::DecisionEngine()
-: m_state(INIT), m_path(nullptr), m_road_builder(nullptr)
 {
+    m_state = Init::instance();
+}
 
+
+void DecisionEngine::change_state(DecisionEngineState* state)
+{
+    m_state = state;
 }
 
 
 void DecisionEngine::update()
 {
-    switch(m_state)
+    m_state->update(this);
+}
+
+
+void DecisionEngineState::update(DecisionEngine* decision_engine)
+{
+
+}
+
+
+void DecisionEngineState::change_state(DecisionEngine* decision_engine, DecisionEngineState* state)
+{
+    decision_engine->change_state(state);
+}
+
+
+Init* Init::_instance = nullptr;
+
+
+DecisionEngineState* Init::instance()
+{
+    if(_instance == nullptr)
     {
-    case INIT:
-    {
-        // For testing, get some free money
-        get_money(100000000);
-
-        // Set company name
-        rename_company("Empire Transport");
-
-        // Choose two random towns and create a new Path object between them
-        Town* town1 = Town::GetRandom();
-        Town* town2 = Town::GetRandom();
-
-        // If the second town is the same as the first, choose a different second town
-        while(town2 == town1)
-        {
-            town2 = Town::GetRandom();
-        }
-
-        print_town_name(town1);
-        print_town_name(town2);
-
-        TileIndex town1_location = town1->xy;
-        TileIndex town2_location = town2->xy;
-
-        if(m_path == nullptr)
-        {
-            m_path = new Path(town1_location, town2_location);
-        }
-
-        std::cout << "\nFinding path...!" << std::flush;
-        m_state = FIND_PATH;
-        break;
-    }
-    case FIND_PATH:
-    {
-        Path::Status find_status = m_path->find(100);
-        if(find_status == Path::FOUND)
-        {
-            std::cout << "\nBuilding road..." << std::flush;
-            m_state = BUILD_ROAD;
-        }
-        if(find_status == Path::UNREACHABLE)
-        {
-            std::cout << "\nDestination unreachable!" << std::flush;
-            m_state = DONE;
-        }
-        break;
-    }
-    case BUILD_ROAD:
-        if(m_road_builder == nullptr)
-        {
-            m_road_builder = new RoadBuilder(*m_path);
-        }
-
-        for(uint8_t count = 0; count < 2; count++)
-        {
-            if(m_road_builder->build_road_segment())
-            {
-                std::cout << "\nConstruction complete!" << std::flush;
-                m_state = DONE;
-                break;
-            }
-        }
-
-        break;
-    case DONE:
-        if(m_path != nullptr)
-        {
-            delete m_path;
-            m_path = nullptr;
-        }
-        if(m_road_builder != nullptr)
-        {
-            delete m_road_builder;
-            m_road_builder = nullptr;
-        }
-        break;
-    default:
-        break;
+        _instance = new Init();
     }
 
-    return;
+    return _instance;
+}
+
+
+void Init::update(DecisionEngine* decision_engine)
+{
+    std::cout << "\nInit" << std::flush;
+
+    // For testing, get some free money
+    get_money(100000000);
+
+    // Set company name
+    rename_company("Empire Transport");
+
+    // Choose two random towns and create a new Path object between them
+    Town* town1 = Town::GetRandom();
+    Town* town2 = Town::GetRandom();
+
+    // If the second town is the same as the first, choose a different second town
+    while(town2 == town1)
+    {
+        town2 = Town::GetRandom();
+    }
+
+    print_town_name(town1);
+    print_town_name(town2);
+
+    TileIndex town1_location = town1->xy;
+    TileIndex town2_location = town2->xy;
+
+    std::cout << "\nFinding path" << std::flush;
+
+    FindPath* find_path = static_cast<FindPath*>(FindPath::instance());
+    find_path->set_source_and_destination(town1_location, town2_location);
+    change_state(decision_engine, find_path);
+}
+
+
+FindPath* FindPath::_instance = nullptr;
+
+
+FindPath::FindPath()
+{
+    m_path = nullptr;
+}
+
+
+DecisionEngineState* FindPath::instance()
+{
+    if(_instance == nullptr)
+    {
+        _instance = new FindPath();
+    }
+
+    return _instance;
+}
+
+
+void FindPath::set_source_and_destination(TileIndex source, TileIndex destination)
+{
+    if(m_path != nullptr)
+    {
+        delete m_path;
+    }
+
+    m_path = new Path(source, destination);
+}
+
+
+void FindPath::update(DecisionEngine* decision_engine)
+{
+    Path::Status find_status = m_path->find(100);
+    if(find_status == Path::FOUND)
+    {
+        std::cout << "\nPath found, building road" << std::flush;
+
+        BuildRoad* build_road = static_cast<BuildRoad*>(BuildRoad::instance());
+        build_road->set_path(m_path);
+        change_state(decision_engine, build_road);
+    }
+    if(find_status == Path::UNREACHABLE)
+    {
+        std::cout << "\nDestination unreachable" << std::flush;
+        change_state(decision_engine, Init::instance());
+    }
+}
+
+
+BuildRoad* BuildRoad::_instance = nullptr;
+
+
+BuildRoad::BuildRoad()
+{
+    m_road_builder = nullptr;
+}
+
+
+DecisionEngineState* BuildRoad::instance()
+{
+    if(_instance == nullptr)
+    {
+        _instance = new BuildRoad();
+    }
+
+    return _instance;
+}
+
+
+void BuildRoad::set_path(Path* path)
+{
+    if(m_road_builder != nullptr)
+    {
+        delete m_road_builder;
+    }
+
+    m_road_builder = new RoadBuilder(*path);
+}
+
+
+void BuildRoad::update(DecisionEngine* decision_engine)
+{
+    for(uint8_t count = 0; count < 2; count++)
+    {
+        if(m_road_builder->build_road_segment())
+        {
+            std::cout << "\nConstruction complete!" << std::flush;
+            change_state(decision_engine, Init::instance());
+            break;
+        }
+    }
 }
--- src/ai/decision_engine.hh
+++ src/ai/decision_engine.hh
@@ -9,29 +9,95 @@
 namespace EmpireAI
 {
 
+    class DecisionEngineState;
+
     class DecisionEngine
     {
     public:
 
         DecisionEngine();
-
         void update();
 
     private:
 
-        enum State
-        {
-            INIT,
-            FIND_PATH,
-            BUILD_ROAD,
-            DONE
-        } m_state;
+        friend class DecisionEngineState;
+        void change_state(DecisionEngineState* state);
+
+        DecisionEngineState* m_state;
+    };
+
+
+    class DecisionEngineState
+    {
+    public:
+
+        virtual ~DecisionEngineState(){}
+
+        virtual void update(DecisionEngine* decision_engine);
+
+    protected:
+
+        void change_state(DecisionEngine* decision_engine, DecisionEngineState* state);
+    };
+
+
+    class Init : public DecisionEngineState
+    {
+    public:
+
+        static DecisionEngineState* instance();
+        void update(DecisionEngine* decision_engine);
+
+    protected:
+
+        Init(){}
+
+    private:
+
+        static Init* _instance;
+    };
+
+
+    class FindPath : public DecisionEngineState
+    {
+    public:
+
+        static DecisionEngineState* instance();
+        void update(DecisionEngine* decision_engine);
+
+        void set_source_and_destination(TileIndex source, TileIndex destination);
+
+    protected:
+
+        FindPath();
+
+    private:
+
+        static FindPath* _instance;
 
         Path* m_path;
+    };
+
+
+    class BuildRoad : public DecisionEngineState
+    {
+    public:
+
+        static DecisionEngineState* instance();
+        void update(DecisionEngine* decision_engine);
+
+        void set_path(Path* path);
+
+    protected:
+
+        BuildRoad();
+
+    private:
+
+        static BuildRoad* _instance;
 
         RoadBuilder* m_road_builder;
     };
-
 }
 
 
